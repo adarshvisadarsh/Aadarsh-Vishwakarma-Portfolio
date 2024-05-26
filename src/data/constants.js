8221ad0f94cd7280642c66bdd4b8cb77c2eddbd0
Aadarsh Vishwakarma <adarshvisadarsh@gmail.com>
2024-05-26 19:38:26 +0530

    Updated Resume link

--- src/data/constants.js
+++ src/data/constants.js
@@ -11,7 +11,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/adarshvisadarsh",
   resume:
-    "https://drive.google.com/file/d/1txFKZD3LNIXMc329jD9c995AU8LItOXK/view?usp=drive_link",
+    "https://drive.google.com/file/d/18I1mOOx0QR4uqXZwqu_UkXzjc5M3cN53/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/aadarsh-vishwakarma/",
   twitter: "https://x.com/aadarsh60843580",
   insta: "https://www.instagram.com/adarshv_07",
